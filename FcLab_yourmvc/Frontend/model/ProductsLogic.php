7b6d73a31ca55413b23595830f78ceb6862ecabf
bobJL <42935115+bobJL@users.noreply.github.com>
2019-06-19 11:01:15 +0200

    revert and recovery

--- FcLab_yourmvc/Frontend/model/ProductsLogic.php
+++ FcLab_yourmvc/Frontend/model/ProductsLogic.php
@@ -37,7 +37,7 @@ class ProductsLogic
 
     public function readContacts(){
         try {
-            $sql = "SELECT * FROM activiteiten;";
+            $sql = "SELECT activiteit_naam, activiteit_benodigdheden, activiteit_beschrijving, 	acitviteit_vrijwilligers FROM activiteiten;";
             $result = $this->DataHandler->readsData($sql);
 
             return $result;
--- FcLab_yourmvc/Frontend/view/old/footer.php
+++ FcLab_yourmvc/Frontend/view/old/footer.php
@@ -1,1 +1,6 @@
-<p>Dit is de footer</p>
+<footer class="py-5 bg-dark col-12" >
+    <div class="container col-12">
+      <p class="m-0 text-center text-white">Copyright &copy;FCL 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
