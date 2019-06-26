a925e25adad5870c348a52e4cc464906974ad71f
bobJL <42935115+bobJL@users.noreply.github.com>
2019-06-26 11:16:49 +0200

    Update index.php

--- FcLab_yourmvc/Frontend/index.php
+++ FcLab_yourmvc/Frontend/index.php
@@ -6,7 +6,7 @@ require_once 'controller/ProductsController.php';
 
 require_once 'utility/utility.php';
 
-require_oncem 'controller/login.controller.php'
+require_once 'controller/login.controller.php'
 
 
 $controller = new ProductsController();
