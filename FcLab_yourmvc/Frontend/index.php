b709174c1b9c8fdbd867a418686b1042a4bc8d5e
anass0347 <50557143+anass0347@users.noreply.github.com>
2019-06-26 11:16:01 +0200

    login werkend

--- FcLab_yourmvc/Frontend/index.php
+++ FcLab_yourmvc/Frontend/index.php
@@ -6,6 +6,8 @@ require_once 'controller/ProductsController.php';
 
 require_once 'utility/utility.php';
 
+require_oncem 'controller/login.controller.php'
+
 
 $controller = new ProductsController();
 $controller->handleRequest();
